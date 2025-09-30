c45e0e32733db358b2a88db46516120a3edd4dc7
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-09-30 09:44:14 +0200

    Dodaj pliki projektów.

---
+++ MauiApp9/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp9
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<FoodItem> Food { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent(); 
+
+            Food = new List<FoodItem>
+            {
+                new FoodItem { Name = "Pizza", Description = "Włoski klasyk z serem i sosem pomidorowym" },
+                new FoodItem { Name = "Burger", Description = "Wołowina, bułka i dodatki" },
+                new FoodItem { Name = "Sushi", Description = "Ryż i świeża ryba" },
+                new FoodItem { Name = "Sałatka", Description = "Zdrowa mieszanka warzyw" }
+            };
+
+            BindingContext = this; 
+        }
+    }
+
+    public class FoodItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
